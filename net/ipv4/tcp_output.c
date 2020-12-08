c8072529f7dbd26caa37a896225ddc39062ab4dd
Yu Deguchi <deguchi00net@s.okayama-u.ac.jp>
2020-12-08 17:35:42 +0900

    [fix]fix rcv_wnd
    rcv_wndの書き方を間違えていたので修正

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2269,7 +2269,7 @@ static bool tcp_small_queue_check(struct sock *sk, const struct sk_buff *skb,
         limit = sock_net(sk)->ipv4.sysctl_tcp_limit_output_bytes;
     }
     if(tp->mpc){
-        mptcp_debug("meta= %p pi= %u cwnd= %u srtt= %u thresh= %u rwnd= %u packetsout= %u pacingrate= %u shiftpacing= %u wmemalloc= %u limit= %u\n", tp->meta_sk, tp->mptcp->path_index, tp->snd_cwnd, (tp->srtt_us>>3) ,tp->snd_ssthresh, tp->rcv->wnd tp->packets_out,sk->sk_pacing_rate,sk->sk_pacing_rate >> 10,refcount_read(&sk->sk_wmem_alloc),limit);
+        mptcp_debug("meta= %p pi= %u cwnd= %u srtt= %u thresh= %u rwnd= %u packetsout= %u pacingrate= %u shiftpacing= %u wmemalloc= %u limit= %u\n", tp->meta_sk, tp->mptcp->path_index, tp->snd_cwnd, (tp->srtt_us>>3) ,tp->snd_ssthresh, tp->rcv_wnd tp->packets_out,sk->sk_pacing_rate,sk->sk_pacing_rate >> 10,refcount_read(&sk->sk_wmem_alloc),limit);
     }else{
         printk("meta= %p pi= 1 cwnd= %u srtt= %u thresh= %u packetsout= %u pacingrate= %u shiftpacing= %u wmemalloc= %u limit= %u\n",sk,tp->snd_cwnd,(tp->srtt_us>>3),tp->snd_ssthresh,tp->packets_out,sk->sk_pacing_rate,sk->sk_pacing_rate >> 10,refcount_read(&sk->sk_wmem_alloc),limit); 
     }
